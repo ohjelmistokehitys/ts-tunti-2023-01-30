bf8a1e95aaedb6041c99f4e4631fabb91ef2d1bd
Teemu H <44917122+swd1tn002@users.noreply.github.com>
2023-01-30 08:10:18 +0000

    Kohta 2 ratkaistu

--- src/postalcodes.ts
+++ src/postalcodes.ts
@@ -1,24 +1,48 @@
 import { readFileSync } from 'fs';
 import path from 'path';
 
-// you can generate the relative path to the CSV that is in the parent folder of this file:
-const csvFile: string = path.join(__dirname, '..', 'postalcodes.csv');
 
-// file can be read into a string with the `readFileSync` function:
-let fileContents: string = readFileSync(csvFile, 'utf-8');
+interface PostOffice {
+    code: string;
+    name: string;
+}
 
+function readPostOffices(): PostOffice[] {
+    // you can generate the relative path to the CSV that is in the parent folder of this file:
+    const csvFile: string = path.join(__dirname, '..', 'postalcodes.csv');
 
-// the string can be split into lines with `split`:
-let lines: string[] = fileContents.trim().split(/\r?\n/);
+    // file can be read into a string with the `readFileSync` function:
+    let fileContents: string = readFileSync(csvFile, 'utf-8');
+
+    // the string can be split into lines with `split`:
+    let lines: string[] = fileContents.trim().split(/\r?\n/);
+
+    // make an array of PostOffice objects and return it
+    return lines.map(line => {
+        let [code, name] = line.split(',');
+        return { code, name };
+    });
+}
+
+// https://stackoverflow.com/a/24457420
+function isNumeric(value: string) {
+    return /^-?\d+$/.test(value);
+}
 
 // you can access command line arguments via `process.argv` variable:
 let params: string[] = process.argv;
-let input = params.at(-1)!.toUpperCase();
+let input = params.at(-1)!;
 
-for (let line of lines) {
-    let parts = line.split(',');
+let postOffices = readPostOffices();
 
-    if (parts[0] === input) {
-        console.log(parts[1]);
-    }
+if (isNumeric(input)) {
+    /* kertoo postitoimipaikan nimen, kun sille annetaan parametrina postinumero */
+    let found = postOffices.find(office => office.code === input);
+    console.log(found?.name);
+} else {
+    /* listaa kaikki annettuun nimeen liittyvät postinumerot samalla rivillä kasvavassa järjestyksessä */
+    let matches = postOffices.filter(office => office.name.toLowerCase() === input.toLowerCase());
+    let codes: string[] = matches.map(office => office.code);
+    codes.sort();
+    console.log(codes.join(', '));
 }
