413a8d38530024e872b279fdd5bd5167b321fc16
Teemu H <44917122+swd1tn002@users.noreply.github.com>
2023-01-30 06:53:21 +0000

    Saving files before refreshing line endings

--- src/postalcodes.ts
+++ src/postalcodes.ts
@@ -1,50 +1,22 @@
 import { readFileSync } from 'fs';
 import path from 'path';
 
-const csvFile = path.join(__dirname, '..', 'postalcodes.csv');
+// you can generate the relative path to the CSV that is in the parent folder of this file:
+const csvFile: string = path.join(__dirname, '..', 'postalcodes.csv');
 
-interface PostOffice {
-    readonly name: string,
-    readonly code: string
-}
+// file can be read into a string with the `readFileSync` function:
+let fileContents: string = readFileSync(csvFile, 'utf-8');
 
-function readPostOffices(csvFile: string): PostOffice[] {
-    let fileContents = readFileSync(csvFile, 'utf-8');
-    let lines: string[] = fileContents.trim().split('\n');
 
-    return lines.map(line => {
-        let [code, name] = line.split(',');
-        return { code, name };
-    });
-}
+// the string can be split into lines with `split`:
+let lines: string[] = fileContents.trim().split(/\r?\n/);
 
-function isNumeric(value?: string): boolean {
-    return Number.isInteger(Number(value));
-}
+console.log('The first 5 lines read from CSV file:');
+console.table(lines.slice(0, 5));
 
-function main(): void {
-    const param = process.argv.at(2);
-    if (param === undefined) {
-        console.log(`Please add the postal code or name as parameter.`);
-        return;
-    }
 
-    const offices = readPostOffices(csvFile);
+// you can access command line arguments via `process.argv` variable:
+let params: string[] = process.argv;
 
-    if (isNumeric(param)) {
-        let found = offices.find(office => office.code === param);
-
-        console.log(found?.name);
-
-    } else {
-        let findName = param.toLowerCase();
-        let filtered = offices.filter(office => office.name.toLowerCase() === findName);
-        let codes = filtered.map(office => office.code);
-
-        console.log(codes.sort().join(', '));
-    }
-}
-
-if (require.main === module) {
-    main();
-}
+console.log('The contents of the `process.argv` array:');
+console.table(params);
